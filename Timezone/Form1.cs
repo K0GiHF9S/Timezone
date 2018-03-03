61decc039f5c89236face86a60aed803a47641bc
H/S <kidoairakusyunkasyuutou@gmail.com>
2018-03-03 23:57:18 +0900

    packaging

--- Timezone/Form1.cs
+++ Timezone/Form1.cs
@@ -11,8 +11,7 @@ namespace Timezone
 {
     public partial class TimeZoneSetting : Form
     {
-        private Dictionary<string, TIME_ZONE_INFORMATION> timeZoneList;
-        private TimeZone currentTimeZone = TimeZone.CurrentTimeZone;
+        private string currentTimeZoneId;
 
         public TimeZoneSetting()
         {
@@ -21,47 +20,26 @@ namespace Timezone
 
         private void TimeZoneSetting_Load(object sender, EventArgs e)
         {
-            var keyName = @"Software\Microsoft\Windows NT\CurrentVersion\Time Zones";
-            using (var registryKey = Registry.LocalMachine.OpenSubKey(keyName))
+            var timeZoneIdList = TimeZoneInfo.GetTimeZoneIdList();
+            this.timeZoneComboBox.Items.Clear();
+            foreach (var timeZoneId in timeZoneIdList)
             {
-                var subKeyNameList = new List<string>(registryKey.GetSubKeyNames());
-                subKeyNameList.Sort();
-
-                this.timeZoneComboBox.Items.Clear();
-                timeZoneList = new Dictionary<string, TIME_ZONE_INFORMATION>();
-                var currentStandardName = currentTimeZone.StandardName;
-                foreach (var subKeyName in subKeyNameList)
-                {
-                    this.timeZoneComboBox.Items.Add(subKeyName);
-
-                    using (var subKey = registryKey.OpenSubKey(subKeyName))
-                    {
-                        var standardName = subKey.GetValue("Std").ToString();
-                        var daylightName = subKey.GetValue("Dlt").ToString();
-                        var tzi = (byte[])subKey.GetValue("TZI");
-                        var timeZoneInformation = new TIME_ZONE_INFORMATION(tzi, standardName, daylightName);
-                        timeZoneList.Add(subKeyName, timeZoneInformation);
-                        if (currentStandardName == standardName)
-                        {
-                            this.timeZoneComboBox.SelectedIndex = timeZoneList.Count - 1;
-                        }
-                    }
-                }
+                this.timeZoneComboBox.Items.Add(timeZoneId);
             }
             
-            var dateTimeOffset = DateTimeOffset.Now;
+            currentTimeZoneId = TimeZoneInfo.GetCurrentTimeZoneId();
+            this.timeZoneComboBox.SelectedIndex = timeZoneIdList.IndexOf(currentTimeZoneId);
         }
 
         private void okButton_Click(object sender, EventArgs e)
         {
-            TIME_ZONE_INFORMATION selectedTimeZone;
             var selectedString = this.timeZoneComboBox.SelectedItem.ToString();
-            var result =
-                timeZoneList.TryGetValue(selectedString, out selectedTimeZone);
-            if (result && currentTimeZone.StandardName != selectedTimeZone.StandardName)
+            if (selectedString != null && selectedString != currentTimeZoneId)
             {
-                TimeZoneInfo.SetTimeZoneInformation(timeZoneList[selectedString]);
-                currentTimeZone = TimeZone.CurrentTimeZone;
+                if (TimeZoneInfo.SetTimeZoneInformation(selectedString) == 0)
+                {
+                    currentTimeZoneId = selectedString;
+                }
             }
         }
     }
--- Timezone/TimeZoneInfo.cs
+++ Timezone/TimeZoneInfo.cs
@@ -1,95 +1,96 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Timezone
 {
-    [StructLayout(LayoutKind.Sequential)]
-    struct SYSTEMTIME
+    static class TimeZoneInfo
     {
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wYear;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wMonth;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wDayOfWeek;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wDay;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wHour;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wMinute;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wSecond;
-        [MarshalAs(UnmanagedType.U2)]
-        public Int16 wMilliseconds;
-        public SYSTEMTIME(byte[] byteArray)
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SYSTEMTIME
         {
-            if (byteArray == null || byteArray.Length != 0x10)
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wYear;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wMonth;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wDayOfWeek;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wDay;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wHour;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wMinute;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wSecond;
+            [MarshalAs(UnmanagedType.U2)]
+            public Int16 wMilliseconds;
+            public SYSTEMTIME(byte[] byteArray)
             {
-                throw new ArgumentException();
+                if (byteArray == null || byteArray.Length != 0x10)
+                {
+                    throw new ArgumentException();
+                }
+                int offset = 0;
+                this.wYear = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wMonth = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wDayOfWeek = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wDay = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wHour = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wMinute = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wSecond = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
+                this.wMilliseconds = BitConverter.ToInt16(byteArray, offset);
+                offset += 0x02;
             }
-            int offset = 0;
-            this.wYear = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wMonth = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wDayOfWeek = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wDay = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wHour = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wMinute = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wSecond = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
-            this.wMilliseconds = BitConverter.ToInt16(byteArray, offset);
-            offset += 0x02;
         }
-    }
 
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    struct TIME_ZONE_INFORMATION
-    {
-        [MarshalAs(UnmanagedType.I4)]
-        public Int32 Bias;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
-        public string StandardName;
-        public SYSTEMTIME StandardDate;
-        [MarshalAs(UnmanagedType.I4)]
-        public Int32 StandardBias;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
-        public string DaylightName;
-        public SYSTEMTIME DaylightDate;
-        [MarshalAs(UnmanagedType.I4)]
-        public Int32 DaylightBias;
-
-        public TIME_ZONE_INFORMATION(byte[] byteArray, string StandardName, string DaylightName)
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct TIME_ZONE_INFORMATION
         {
-            if (byteArray == null || byteArray.Length != 0x2c)
+            [MarshalAs(UnmanagedType.I4)]
+            public Int32 Bias;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string StandardName;
+            public SYSTEMTIME StandardDate;
+            [MarshalAs(UnmanagedType.I4)]
+            public Int32 StandardBias;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string DaylightName;
+            public SYSTEMTIME DaylightDate;
+            [MarshalAs(UnmanagedType.I4)]
+            public Int32 DaylightBias;
+
+            public TIME_ZONE_INFORMATION(byte[] byteArray, string StandardName, string DaylightName)
             {
-                throw new ArgumentException();
+                if (byteArray == null || byteArray.Length != 0x2c)
+                {
+                    throw new ArgumentException();
+                }
+                int offset = 0;
+                this.Bias = BitConverter.ToInt32(byteArray, offset);
+                offset += 0x04;
+                this.StandardBias = BitConverter.ToInt32(byteArray, offset);
+                offset += 0x04;
+                this.DaylightBias = BitConverter.ToInt32(byteArray, offset);
+                offset += 0x04;
+                this.StandardDate = new SYSTEMTIME((new List<byte>(byteArray).GetRange(offset, 0x10)).ToArray());
+                offset += 0x10;
+                this.DaylightDate = new SYSTEMTIME((new List<byte>(byteArray).GetRange(offset, 0x10)).ToArray());
+
+                this.StandardName = StandardName;
+                this.DaylightName = DaylightName;
             }
-            int offset = 0;
-            this.Bias = BitConverter.ToInt32(byteArray, offset);
-            offset += 0x04;
-            this.StandardBias = BitConverter.ToInt32(byteArray, offset);
-            offset += 0x04;
-            this.DaylightBias = BitConverter.ToInt32(byteArray, offset);
-            offset += 0x04;
-            this.StandardDate = new SYSTEMTIME((new List<byte>(byteArray).GetRange(offset, 0x10)).ToArray());
-            offset += 0x10;
-            this.DaylightDate = new SYSTEMTIME((new List<byte>(byteArray).GetRange(offset, 0x10)).ToArray());
-
-            this.StandardName = StandardName;
-            this.DaylightName = DaylightName;
         }
-    }
 
-    class TimeZoneInfo
-    {
         [StructLayout(LayoutKind.Sequential)]
         private struct TOKEN_PRIVILEGES
         {
@@ -103,7 +104,7 @@ namespace Timezone
 
 
         [StructLayout(LayoutKind.Sequential)]
-        internal struct LUID
+        private struct LUID
         {
             [MarshalAs(UnmanagedType.U4)]
             public uint LowPart;
@@ -128,17 +129,62 @@ namespace Timezone
             [In] ref TOKEN_PRIVILEGES NewState, Int32 BufferLength, Int32 PreviousState, Int32 ReturnLength);
 
         [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
-        public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+        private static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
 
-        public const UInt32 TOKEN_QUERY = 0x08;
-        public const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x20;
-        public const UInt32 SE_PRIVILEGE_ENABLED = 0x02;
-        public const string SE_TIME_ZONE_PRIVILEGE = "SeTimeZonePrivilege";
+        private const UInt32 TOKEN_QUERY = 0x08;
+        private const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x20;
+        private const UInt32 SE_PRIVILEGE_ENABLED = 0x02;
+        private const string SE_TIME_ZONE_PRIVILEGE = "SeTimeZonePrivilege";
+        
+        private static readonly Dictionary<string, TIME_ZONE_INFORMATION> timeZoneDictionary;
+        private static readonly Dictionary<string, string> standardNameTimeZoneIdDictionary;
 
-        public static int SetTimeZoneInformation(TIME_ZONE_INFORMATION tzi)
+        static TimeZoneInfo()
         {
+            var keyName = @"Software\Microsoft\Windows NT\CurrentVersion\Time Zones";
+            using (var registryKey = Registry.LocalMachine.OpenSubKey(keyName))
+            {
+                var subKeyNameList = new List<string>(registryKey.GetSubKeyNames());
+                subKeyNameList.Sort();
+                
+                timeZoneDictionary = new Dictionary<string, TIME_ZONE_INFORMATION>();
+                standardNameTimeZoneIdDictionary = new Dictionary<string, string>();
+                foreach (var subKeyName in subKeyNameList)
+                {
+                    using (var subKey = registryKey.OpenSubKey(subKeyName))
+                    {
+                        var standardName = subKey.GetValue("Std").ToString();
+                        var daylightName = subKey.GetValue("Dlt").ToString();
+                        var tzi = (byte[])subKey.GetValue("TZI");
+                        var timeZoneInformation = new TIME_ZONE_INFORMATION(tzi, standardName, daylightName);
+                        timeZoneDictionary.Add(subKeyName, timeZoneInformation);
+                        standardNameTimeZoneIdDictionary.Add(standardName, subKeyName);
+                    }
+                }
+            }
+        }
+
+        public static List<string> GetTimeZoneIdList()
+        {
+            return new List<string>(timeZoneDictionary.Keys);
+        }
+
+        public static string GetCurrentTimeZoneId()
+        {
+            return standardNameTimeZoneIdDictionary[TimeZone.CurrentTimeZone.StandardName];
+        }
+
+        public static int SetTimeZoneInformation(string timeZoneId)
+        {
+            var result =
+                timeZoneDictionary.TryGetValue(timeZoneId, out TIME_ZONE_INFORMATION selectedTimeZone);
+            if (!result)
+            {
+                return -1;
+            }
+
             EnablePrivileges();
-            var result = SetTimeZoneInformation(ref tzi);
+            result = SetTimeZoneInformation(ref selectedTimeZone);
 
             var lastError = Marshal.GetLastWin32Error();
 
